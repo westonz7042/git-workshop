703a97a93dad3f81ac83d1ee6eff9693a17cb6c1
kevinsun0904 <kevinsun0904@gmail.com>
2024-11-11 16:13:02 -0800

    Added java and python debugging exercise

---
+++ SomeJavaCode.java
@@ -0,0 +1,8 @@
+public class SomeJavaCode {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 3, 6, 3, 10, 20 };
+        for (int i = 0; i <= arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ some_python_code.py
@@ -0,0 +1,3 @@
+arr = [1, 3, 6, 3, 10, 20]
+for i in range(len(arr) + 1):
+    print(arr[i])
