24e26d97b88c0bf81cc1506121610c160f8fcee5
westonz7042 <quadruple.ww@gmail.com>
2024-11-12 20:24:56 -0800

    update python and java files

--- SomeJavaCode.java
+++ SomeJavaCode.java
@@ -1,7 +1,7 @@
 public class SomeJavaCode {
     public static void main(String[] args) {
         int[] arr = new int[] { 1, 3, 6, 3, 10, 20 };
-        for (int i = 0; i <= arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
--- some_python_code.py
+++ some_python_code.py
@@ -1,3 +1,3 @@
 arr = [1, 3, 6, 3, 10, 20]
-for i in range(len(arr) + 1):
+for i in range(len(arr)):
     print(arr[i])
